ff47a9132255f2aad2921f738d9d541926bfd6be
AJ Haibe <aj@AJs-MacBook-Pro.local>
2022-12-14 20:51:33 +0000

    initail

---
+++ js/script.js
@@ -0,0 +1,87 @@
+var searchInput =  document.querySelector('.search');
+var cardWrapper = document.querySelector('main');
+
+function noMatch() {
+    cardWrapper.innerHTML = '<p class="no-search">No results found </p>'
+}
+
+function displayMatches (matches) {
+    cardWrapper.innerHTML = '';
+
+if (matches.length === 0) {
+    noMatch()
+}
+
+   for (var matchObj of matches) {
+    cardWrapper.insertAdjacentHTML('beforeend', `
+    <div class="movie-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), 
+    url(${matchObj.movie_image});">
+    <h3>${matchObj.title}</h3>
+    <p>${matchObj.description}</p>
+    <a href="${matchObj.imdb_link}" target='_blank'>View more info</a>
+    </div> `)
+    
+   }
+
+}
+
+
+function fetchMovies (event){   
+    var keyCode = event.keyCode;
+    var searchText = searchInput.value.toLowerCase().trim();
+
+    if (keyCode === 13 && searchText) {
+        var matches = [];
+
+        for (var movieObj of movieData) {
+            if ( movieObj.title.toLocaleLowerCase().includes(searchText)){
+                matches.push(movieObj);
+               
+            }
+           
+
+        }
+        searchInput.value = '';
+        displayMatches(matches);
+        fetch().then(function(responseObj){
+            var dataPromise = responseObj.json();
+
+            dataPromise.then(function(data){
+
+            });
+        });
+        
+    }
+}
+
+
+function init() {
+    searchInput.addEventListener('keydown', fetchMovies);
+}
+
+init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
